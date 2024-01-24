bc3973b5195267cd622904da5a79065645d2c067
Rana Das <ranadas23112002@gmail.com>
2024-01-24 06:39:20 +0530

    Updated

--- Humanoid.sh
+++ Humanoid.sh
@@ -1,8 +1,16 @@
-cd frontend/
+echo "Starting Frontend..."
 npm run dev
-cd ..
-./run_chatbot_endpoint.sh &
-PID_RASA=$!
-sleep 10
-./run_ocr_endpoint.sh
-kill $PID_RASA
+sleep 5
+
+if [ $? -eq 0 ]; then
+    echo "Starting Rasa Backend..."
+    ./run_chatbot_endpoint.sh
+    if [ $? -eq 0 ]; then
+        echo "Starting OCR Endpoint..."
+        ./run_ocr_endpoint.sh
+    else
+        echo "Error: Rasa Backend failed to start."
+    fi
+else
+    echo "Error: Frontend failed to start."
+fi
